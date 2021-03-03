90f80101f1c944c824ab5cc7b215fed26899b953
svenzu6 <zubcic.sven@gmail.com>
2021-03-03 15:34:20 +0100

    featuer(Register): implement react-hook-form

--- src/modules/Register/Register.jsx
+++ src/modules/Register/Register.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 
 import {
     Button,
     Field,
-    RegisterForm,
+    Form,
     RegisterRoot,
+    RegisterTitle,
 } from './Register.styles'
 
 export const Register = () => {
+    const { errors, handleSubmit, register, watch } = useForm()
+    // eslint-disable-next-line no-console
+    const onSubmit = (data) => console.log(data)
+
     return (
         <RegisterRoot>
-            <RegisterForm>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <RegisterTitle>
+                    4Later
+                </RegisterTitle>
+                <Field
+                    name="username"
+                    placeholder="create username..."
+                    ref={register}
+                    type="username"
+                />
+                <Field
+                    name="email"
+                    placeholder="your email..."
+                    ref={register}
+                    type="email"
+                />
+                <Field
+                    name="password"
+                    placeholder="your password..."
+                    ref={register}
+                    type="password"
+                />
+                <Field
+                    name="password2"
+                    placeholder="confirm your password..."
+                    ref={register}
+                    type="password"
+                />
 
-                <Field placeholder="your email..." />
-                <Field placeholder="your password..." />
-                <Field placeholder="confirm your password..." />
-
-                <Button>
-                    Sign Up
-                </Button>
-            </RegisterForm>
+                <Button
+                    name="submitbtn"
+                    type="submit"
+                />
+            </Form>
         </RegisterRoot>
     )
 }
--- src/modules/Register/Register.styles.js
+++ src/modules/Register/Register.styles.js
@@ -8,16 +8,19 @@ export const RegisterRoot = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    background-color:papayawhip;
 `
 
-export const RegisterForm = styled.div`
-    background-color: red;
+export const Form = styled.form`
+    background-color: pink;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     row-gap: 10px;
     padding:50px;
+    border-radius:3px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 `
 
 export const Field = styled.input`
@@ -28,10 +31,16 @@ export const Field = styled.input`
     border-radius: 3px;
 `
 
-export const Button = styled.div`
+export const Button = styled.input`
     cursor:pointer;
     padding: 0.5em;
-    background: papayawhip;
+    background: gray;
     border: none;
     border-radius: 3px;
+    color:papayawhip;
+    
+`
+export const RegisterTitle = styled.h1`
+color:papayawhip;
+text-decoration-line:overline;
 `
