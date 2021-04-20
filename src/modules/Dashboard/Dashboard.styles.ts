2a15893f97353c7290188464cdab6428a0b8e24e
svenzu6 <zubcic.sven@gmail.com>
2021-04-20 17:12:35 +0200

    ref(Dashboard): component name

--- src/modules/Dashboard/Dashboard.styles.ts
+++ src/modules/Dashboard/Dashboard.styles.ts
@@ -1,21 +1,11 @@
-import { TextField } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography'
 import styled from 'styled-components'
 
-export const TopBar = styled.div`
-    margin-left: 168px;
-    grid-template-rows: 3ch auto minmax(10px, 60px);
-`
-
-export const StyledDialogContent = styled.div`
+export const DashboardDialogContent = styled.div`
     padding: 10px;
 `
 
-export const StyledTextField = styled(TextField)`
-    margin: 100px 0;
-`
-
-export const DialogForm = styled.form`
+export const DashboardDialogForm = styled.form`
     display: grid;
     row-gap: 10px;
     justify-content: center;
@@ -23,7 +13,7 @@ export const DialogForm = styled.form`
     padding: 30px 0;
 `
 
-export const FormTitle = styled(Typography)`
+export const DashboardFormTitle = styled(Typography)`
     align-self: center;
     padding-top: 10px ;
 `
--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -2,17 +2,15 @@ import {
     Button,
     Dialog,
     DialogActions,
-    Toolbar,
+    TextField,
 } from '@material-ui/core'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import React from 'react'
 
 import {
-    DialogForm,
-    FormTitle,
-    StyledDialogContent,
-    StyledTextField,
-    TopBar,
+    DashboardDialogContent,
+    DashboardDialogForm,
+    DashboardFormTitle,
 } from './Dashboard.styles'
 
 export const Dashboard: React.FunctionComponent = () => {
@@ -27,72 +25,68 @@ export const Dashboard: React.FunctionComponent = () => {
     }
 
     return (
-        <TopBar>
-            <Toolbar>
-                <div>
+        <div>
+            <Button
+                color="secondary"
+                onClick={handleClickOpen}
+                variant="outlined"
+            >
+                Add a new link
+            </Button>
+            <Dialog
+                onClose={handleClose}
+                open={open}
+            >
+                <DashboardFormTitle
+                    color="textSecondary"
+                    variant="h6"
+                >
+                    Add a Link
+                </DashboardFormTitle>
+                <DashboardDialogContent>
+                    <DashboardDialogForm>
+                        <TextField
+                            color="secondary"
+                            fullWidth={true}
+                            label="URL"
+                            required={true}
+                            variant="outlined"
+                        />
+                        <TextField
+                            color="secondary"
+                            fullWidth={true}
+                            label="Link Title"
+                            required={true}
+                            variant="outlined"
+                        />
+                        <TextField
+                            color="secondary"
+                            fullWidth={true}
+                            label="Description"
+                            multiline={true}
+                            rows={4}
+                            variant="outlined"
+                        />
+                    </DashboardDialogForm>
+                </DashboardDialogContent>
+                <DialogActions>
                     <Button
                         color="secondary"
-                        onClick={handleClickOpen}
-                        variant="outlined"
+                        endIcon={<KeyboardArrowRightIcon />}
+                        onClick={handleClose}
+                        type="submit"
+                        variant="contained"
                     >
-                        Add a new link
+                        Add
                     </Button>
-                    <Dialog
-                        onClose={handleClose}
-                        open={open}
+                    <Button
+                        color="secondary"
+                        onClick={handleClose}
                     >
-                        <FormTitle
-                            color="textSecondary"
-                            variant="h6"
-                        >
-                            Add a Link
-                        </FormTitle>
-                        <StyledDialogContent>
-                            <DialogForm>
-                                <StyledTextField
-                                    color="secondary"
-                                    fullWidth={true}
-                                    label="URL"
-                                    required={true}
-                                    variant="outlined"
-                                />
-                                <StyledTextField
-                                    color="secondary"
-                                    fullWidth={true}
-                                    label="Link Title"
-                                    required={true}
-                                    variant="outlined"
-                                />
-                                <StyledTextField
-                                    color="secondary"
-                                    fullWidth={true}
-                                    label="Description"
-                                    multiline={true}
-                                    rows={4}
-                                    variant="outlined"
-                                />
-                            </DialogForm>
-                        </StyledDialogContent>
-                        <DialogActions>
-                            <Button
-                                color="secondary"
-                                endIcon={<KeyboardArrowRightIcon />}
-                                onClick={handleClose}
-                                type="submit"
-                                variant="contained"
-                            >
-                                Add
-                            </Button>
-                            <Button
-                                color="secondary"
-                                onClick={handleClose}
-                            >
-                                Cancel
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-                </div>
-            </Toolbar>
-        </TopBar>
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     )
 }
