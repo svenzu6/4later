d1ec4c499081a93081b98e6524e0aa1deea25855
svenzu6 <zubcic.sven@gmail.com>
2021-05-07 16:52:25 +0200

    feat(Favorites): create Favorite modal

---
+++ src/modules/Favorites/Favorites.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const FavoritesRoot = styled.div`
+    padding: 20px;
+`
+export const FavoritesTitle = styled.h1`
+    text-align: center;
+`
---
+++ src/modules/Favorites/Favorites.tsx
@@ -0,0 +1,14 @@
+import {
+    FavoritesRoot,
+    FavoritesTitle,
+} from './Favorites.styles'
+
+export const Favorites: React.FunctionComponent = () => {
+    return (
+        <FavoritesRoot>
+            <FavoritesTitle>
+                FAVORITES
+            </FavoritesTitle>
+        </FavoritesRoot>
+    )
+}
