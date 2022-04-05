519b427f7001b225af001be28d185cc8c1300341
svenzu6 <78984293+svenzu6@users.noreply.github.com>
2022-04-05 15:51:36 +0200

    Update Home.styles.ts
    Remove background images

--- src/modules/Home/Home.styles.ts
+++ src/modules/Home/Home.styles.ts
@@ -7,14 +7,7 @@ export const HomeRoot = styled.div`
     justify-content: center;
     align-items: center;
 `
-export const HomeBackground = styled.div`
-    background-image: url('icons/floppy.jpg');
-    background-position: center;
-    background-repeat: no-repeat;
-    filter: blur(8px);
-    height: 100%;
-    width: 100%;
-`
+
 
 export const HomeContent = styled.div`
     display: flex;
@@ -24,8 +17,7 @@ export const HomeContent = styled.div`
     background-image: url('icons/floppy.jpg');
     background-position: center;
     background-size: cover;
-    height: 80%;
-    width: 60%;
+   
     position: absolute;
     z-index: 2;
     box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
